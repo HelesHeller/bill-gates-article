251ba78c9202fe4acc965b540cea49c750a439be
heles <heles.hals@gmail.com>
2024-09-17 16:12:30 +0300

    one page work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {BillGatesComponent} from "./bill-gates/bill-gates.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BillGatesComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']  // Исправлено с 'styleUrl' на 'styleUrls'
 })
 export class AppComponent {
   title = 'bill-gates-article';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { BillGatesComponent } from './bill-gates/bill-gates.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: BillGatesComponent }  // Подключаем компонент
+];
